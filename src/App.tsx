8b55e67e17c651ade57ae7c357d19ace3b953df7
ons ATIG <onsatig45@gmail.com>
2024-10-26 22:40:36 +0100

    Initial commit with Tic Tac Toe project

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,15 @@
+// src/App.tsx
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Board from './components/Board';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App: React.FC = () => {
+    return (
+        <div className="min-h-screen bg-gradient-to-br from-indigo-50 to-blue-100 flex items-center justify-center p-6">
+            <div className="bg-white p-8 rounded-lg shadow-xl max-w-screen-md w-full">
+                <Board />
+            </div>
+        </div>
+    );
+};
 
 export default App;
